5edd4dcf698ce4a726e3b65c31cb1e7fb290611a
Zuleima Muñoz <zulem090@gmail.com>
2024-09-24 00:27:48 -0500

    instalado paquete de pruebas estaticas y pruebas unitarias (jest/prettier)

--- jest.config.js
+++ jest.config.js
@@ -1,18 +1,24 @@
-const nextJest = require("next/jest");
+const nextJest = require('next/jest');
 
 const createJestConfig = nextJest({
   // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
-  dir: "./src",
+  dir: './src',
 });
 
 // Add any custom config to be passed to Jest
 const customJestConfig = {
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
-  testEnvironment: "jest-environment-jsdom",
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  testEnvironment: 'jest-environment-jsdom',
   coverageReporters: ['json', 'cobertura', 'lcov', 'text'],
   moduleDirectories: ['node_modules', 'src'],
   testMatch: ['<rootDir>/src/**/*.spec.ts?(x)'],
   coverageDirectory: 'coverage/summary',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
   clearMocks: true,
   testTimeout: 20000,
   coverageThreshold: {
